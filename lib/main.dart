3ec68566634cc1a1f58662585b028ab18b83b887
maxim sessler <MaximSessler@mail.de>
2023-12-19 12:07:03 +0100

    erster

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DetailsScreen extends StatelessWidget {
+  const DetailsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue, // Set the color to blue
+      child: // Add your widgets here
+    );
+  }
+}
